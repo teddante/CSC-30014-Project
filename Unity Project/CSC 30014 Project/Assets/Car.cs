718e3dc10ae8b78d3fcef90d45e0b85d61ec5ae7
Ted Enston <edwardenston@gmail.com>
2022-03-18 11:31:15 +0000

    0.1.1

--- Unity Project/CSC 30014 Project/Assets/Car.cs
+++ Unity Project/CSC 30014 Project/Assets/Car.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,12 @@ public class Car : MonoBehaviour
     public AnimationCurve TorqueCurve;
 
     public List<float> Gears = new();
+    public float WheelTorque;
+    public float EngineTorque;
+    public int CurrentGear;
+    public float FinalDifferential;
+    public float RPM;
+    public float Throttle;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,10 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Throttle = Mathf.Clamp(Throttle, 0, 1);
+
+        EngineTorque = Mathf.Clamp((TorqueCurve.Evaluate(RPM) * Throttle), 1000, 7250);
+
+        WheelTorque = EngineTorque * Gears[CurrentGear] * FinalDifferential;
     }
 }
