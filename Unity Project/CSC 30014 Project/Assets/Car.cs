e89bc8c02a5d9d34d72e3f6da3a8bbbbd7c7dab6
Ted Enston <edwardenston@gmail.com>
2022-03-18 11:13:53 +0000

    0.1.0

---
+++ Unity Project/CSC 30014 Project/Assets/Car.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public AnimationCurve TorqueCurve;
+
+    public List<float> Gears = new();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
